8e00772e4af91d666fa6e443d64438450997da22
EdwinYax111 <edwinyax1@gmail.com>
2017-10-06 00:42:45 -0600

    ahora loguea.

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 's*cb8ur5(id%#6y*@@7ftuohqf_!jedcg04+4h3lqj9z06b)a_'
 
+
+LOGIN_REDIRECT_URL = '/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
